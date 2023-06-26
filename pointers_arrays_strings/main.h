a4c62ae2a26aba06a501df9e61c220ea62d895a4
BIDcolonel <ptitofdu89@live.fr>
2023-06-26 03:44:24 -0700

    create 2-strlen.c and compile for 2-strlen

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
