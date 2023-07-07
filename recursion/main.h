4ce43a39841c40d0e341cdf131e8f6833c45cd85
BIDcolonel <ptitofdu89@live.fr>
2023-07-07 05:46:22 -0700

    create 100-is_palindrome.c and modify main.h for add prototype

--- recursion/main.h
+++ recursion/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stddef.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -13,5 +14,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+int is_palindrome_recursive(char *s, int start, int end);
 
 #endif
