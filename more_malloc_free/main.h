44d4dc7394663c7d6c581c2edf51b94bc3b65151
BIDcolonel <ptitofdu89@live.fr>
2023-07-11 00:20:04 -0700

    add library limits.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <ctype.h>
+#include <limits.h>
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
