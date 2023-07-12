da95202794527e12a4e0bc6d4ba1c3dbf289c951
BIDcolonel <ptitofdu89@live.fr>
2023-07-12 01:50:19 -0700

    create 3-array_range.c

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *array_range - creates an array of integers
+ * @min: int type
+ * @max: int type
+ * Return: return pointer to array
+ * If min > max, return NULL
+ * If malloc fails, return NULL
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int arr;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(sizeof(int) * (max - min + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	arr = 0;
+	while (min <= max)
+	{
+		ptr[arr] = min;
+		min++;
+		arr++;
+	}
+	return (ptr);
+}
