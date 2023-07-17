abdf14e83426be597c15e188ac9d7d16c3c3c244
BIDcolonel <ptitofdu89@live.fr>
2023-07-17 02:04:11 -0700

    add all project prototypes and delete putchar

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -7,6 +7,10 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <limits.h>
-int _putchar(char c);
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
