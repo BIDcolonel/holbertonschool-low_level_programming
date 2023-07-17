cb8ab31e4c456b97be4771aee9bcff88e2e70146
BIDcolonel <ptitofdu89@live.fr>
2023-07-17 02:58:28 -0700

    modify main.h, add prototype in dog.h and create 1-init_dog.c

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -7,10 +7,6 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <limits.h>
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+#include <stddef.h>
 
 #endif
