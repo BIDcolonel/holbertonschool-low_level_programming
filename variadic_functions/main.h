5bd53266b03ddc5df8d37c64649aa5db31a7f64c
BIDcolonel <ptitofdu89@live.fr>
2023-07-18 05:31:39 -0700

    create main.h for standard library, variadic_function.h and 0-sum_them_all.c

---
+++ variadic_functions/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <locale.h>
+#include <math.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+int _putchar(char c);
+
+#endif
