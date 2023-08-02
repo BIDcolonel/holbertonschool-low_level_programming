92600704b86a1728c533967fcc60719e28b4e934
BIDcolonel <ptitofdu89@live.fr>
2023-08-02 05:47:08 -0700

    change line declaration for the handled malloc return

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -17,24 +17,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	unsigned int count = 0;
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
 	if (h == NULL)
 		return (NULL);
 
-	if (new_node == NULL)
-		return (NULL);
-
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
 		new_node->next = *h;
-
 		if (*h != NULL)
 			(*h)->prev = new_node;
 		*h = new_node;
 		return (new_node);
 	}
+
 	while (current != NULL)
 	{
 		if (count == idx - 1)
