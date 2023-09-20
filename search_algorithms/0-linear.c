8363588156ca337c28d65897a560dccb42846822
BIDcolonel <ptitofdu89@live.fr>
2023-09-20 14:02:38 +0200

    complete code revision + README complement + main added

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,15 +11,16 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t index;
 
-	if (array == NULL)
-		return -1;
+	if (array == NULL || size < 1)
+		return (-1);
 
 	for (index = 0; index < size; index++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
+		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
+
 		if (array[index] == value)
-			return (int)index;
+			return (index);
 	}
 
-	return -1;
+	return (-1);
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,8 +1,8 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
-#include <stddef.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
